9804bde2b4ec130b94777a37483a71140e2fac0a
Mawada <mawada.alexander1@gmail.com>
2026-05-09 09:08:48 +0300

    added new trainee functions

--- trainee/views.py
+++ trainee/views.py
@@ -18,3 +18,24 @@ def delete_trainee(request, trainee_id):
     trainee = Trainee.objects.get(id=trainee_id)
     trainee.delete()
     return redirect('trainee_list')
+
+def trainee_list(request):
+    trainees = Trainee.objects.all() 
+    return render(request, 'trainee/traineelist.html', {'trainees': trainees})
+
+def trainee_detail(request, trainee_id):
+    trainee = Trainee.objects.get(id=trainee_id)
+    return render(request, 'trainee/trainee_detail.html', {'trainee': trainee})
+
+def insert_trainee(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        Trainee.objects.create(name=name, email=email)
+        return redirect('trainee_list')
+    return render(request, 'trainee/add_trainee.html')
+
+def hard_delete_trainee(request, trainee_id):
+    trainee = Trainee.objects.get(id=trainee_id)
+    trainee.delete()
+    return redirect('trainee_list')
